e1f9efba83f74f874657c79da109b6873e09d778
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-04-02 12:57:45 +0300

    Worked on vertex, added some basic functions

--- Algorithms Assignment/Vertex.h
+++ Algorithms Assignment/Vertex.h
@@ -3,8 +3,18 @@
 #include  <list>
 #include <vector>
 
+using namespace std;
+
 class vertex
 {
 private:
+	int m_value;
+	list<vertex> m_neighbors;
 public:
+	vertex(int i_value) { m_value = i_value; }
+	void add_neighbor(vertex& i_neighbor);
+	bool neighbor_exists(const vertex& i_neighbor) const;
+	int get_value()const { return m_value; }
+	void set_value(int i_value) { m_value = i_value; }
+	bool operator==(const vertex& i_other)const { return i_other.m_value == m_value; }
 };

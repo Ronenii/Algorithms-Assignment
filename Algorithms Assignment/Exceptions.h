f3e94b733ffa3d26312c5937a762ed395245f146
Roy Toledano <royto@mta.ac.il>
2023-04-02 12:18:32 +0300

    create exceptions class

--- Algorithms Assignment/Exceptions.h
+++ Algorithms Assignment/Exceptions.h
@@ -1,7 +1,20 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+using namespace std;
 
-class exceptions
+class someException
 {
-private:
+	string message;
+
 public:
+	someException(const string& input)
+	{
+		message = "\"" + input + "\" does not exist, please try again.";
+	}
+
+	char const* what()const
+	{
+		return message.c_str();
+	}
 };

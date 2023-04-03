e1a2bb1862eb2d409b3ae2cfb1d3a042eb340908
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-04-03 13:51:43 +0300

    continued work on dfs

--- Algorithms Assignment/Graph.cpp
+++ Algorithms Assignment/Graph.cpp
@@ -4,19 +4,22 @@ graph::graph(int i_num_of_vertexes, int i_num_of_edges) : m_num_of_vertexes(i_nu
 {
 	for (int i = 0; i < i_num_of_vertexes; i++)
 	{
-		m_vertexes.push_back(vertex(i+1));
+		m_vertexes.push_back(vertex(i + 1));
 	}
 }
 
-void graph::visit(vertex i_vertex)
+void graph::visit(vertex& i_vertex)
 {
 	i_vertex.set_color(Color::GRAY);
 	list<vertex>& neighbors = i_vertex.get_neighbors();
 	for (auto& neighbor : neighbors)
 	{
 		if (neighbor.get_color() == Color::WHITE)
+		{
 			visit(neighbor);
+		}
 	}
+
 	i_vertex.set_color(Color::BLACK);
 }
 
@@ -27,13 +30,24 @@ bool graph::is_vertex_exists(vertex& i_vertex) const
 		if (vertex == i_vertex)
 			return true;
 	}
+
 	return false;
 }
 
-void graph::DFS()
+bool graph::dfs()
 {
-	for (auto & v : m_vertexes)
+	bool isSingleTree;
+
+	for (auto& v : m_vertexes)
 	{
-		v.set
+		v.set_color(Color::WHITE);
+	}
+
+	for (auto& v : m_vertexes)
+	{
+		if (v.get_color() == Color::WHITE)
+		{
+			visit(v);
+		}
 	}
 }
--- Algorithms Assignment/Graph.h
+++ Algorithms Assignment/Graph.h
@@ -9,21 +9,23 @@ private:
 	vector<vertex> m_vertexes;
 	int m_num_of_vertexes, m_num_of_edges;
 	bool is_vertex_exists(vertex& i_vertex) const;
+	
+
 public:
 	graph(int i_num_of_vertexes, int i_num_of_edges);
 	virtual ~graph() = default;
 	virtual vector<vertex> get_vertexes() const { return m_vertexes; }
 	virtual int get_num_of_vertexes() const { return m_num_of_vertexes; }
 	virtual int get_num_of_edges() const { return m_num_of_edges; }
-	virtual void set_num_of_vertexes(const int i_num_of_vertexes) {m_num_of_vertexes = i_num_of_vertexes;}
+	virtual void set_num_of_vertexes(const int i_num_of_vertexes) { m_num_of_vertexes = i_num_of_vertexes; }
 	virtual void set_num_of_edges(const int i_num_of_edges) { m_num_of_edges = i_num_of_edges; }
 	virtual vertex& get_vertex_by_value(int i_value) { return m_vertexes[i_value - 1]; }
 	virtual void set_edge(vertex& i_src, vertex& i_dst) = 0;
-
+	bool dfs();
 protected:
 	virtual bool is_graph_conected();
-	void visit(vertex i_vertex);
+	void visit(vertex& i_vertex);
 
 
-	
+
 };

4eb25f43334a6d5752e84164f1d21e01fd486d12
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-04-03 12:55:00 +0300

    shabang

--- Algorithms Assignment/IO Manager.cpp
+++ Algorithms Assignment/IO Manager.cpp
@@ -1,5 +1,24 @@
 #include "IO Manager.h"
 
+graph* io_manager::get_user_input()
+{
+	int vertices, edges;
+
+	const bool is_directed = get_is_graph_directed();
+	cin >> vertices >> edges;
+
+	if (is_directed)
+	{
+		directed_graph* dg = new directed_graph(vertices, edges);
+		return dg;
+	}
+	else
+	{
+		non_directed_graph* ndg = new non_directed_graph(vertices, edges);
+		return ndg;
+	}
+}
+
 bool io_manager::get_is_graph_directed()
 {
 	char input;
@@ -12,36 +31,18 @@ bool io_manager::get_is_graph_directed()
 	return true;
 }
 
-graph * io_manager::get_user_input()
+void io_manager::get_directed_graph_input(directed_graph & i_dg)
 {
-	int vertices, edges;
+	int v1, v2;
 
-	const bool is_directed = get_is_graph_directed();
-	cin >> vertices >> edges;
-
-	if(is_directed)
+	for(int i = 0; i < i_dg.get_num_of_edges(); i++)
 	{
-		directed_graph * dg = new directed_graph(vertices, edges);
-		get_graph_input(dg);
-		return dg;
-	}
-	else
-	{
-		non_directed_graph * ndg = new non_directed_graph(vertices, edges);
-		get_graph_input(ndg);
-		return ndg;
+		cin >> v1 >> v2;
+		i_dg.set_edge(i_dg.get_vertex_by_value(v1), i_dg.get_vertex_by_value(v2));
 	}
 }
 
-void io_manager::get_graph_input(graph* i_graph)
+directed_graph io_manager::get_directed_graph_input()
 {
-	int src_value, dst_value;
-
-	for (int i = 0; i < i_graph->get_num_of_edges(); i++)
-	{
-		cin >> src_value >> dst_value;
-		vertex& src = i_graph->get_vertex_by_value(src_value);
-		vertex& dst = i_graph->get_vertex_by_value(dst_value);
-		i_graph->set_edge(src, dst);
-	}
+	
 }

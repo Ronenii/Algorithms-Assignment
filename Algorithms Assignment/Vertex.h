7085547a68a519556a56fc3f57d741096cb8fecc
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-04-03 13:25:46 +0300

    added color

--- Algorithms Assignment/Vertex.h
+++ Algorithms Assignment/Vertex.h
@@ -4,12 +4,14 @@
 #include <vector>
 
 using namespace std;
+enum Color { WHITE, GRAY, BLACK };
 
 class vertex
 {
 private:
 	int m_in_degree = 0, m_out_degree = 0, m_degree = 0;
 	int m_value;
+	Color m_color = WHITE;
 	list<vertex> m_neighbors;
 public:
 	vertex(int i_value = 0) { m_value = i_value; }
@@ -18,4 +20,8 @@ public:
 	int get_value()const { return m_value; }
 	void set_value(int i_value) { m_value = i_value; }
 	bool operator==(const vertex& i_other)const { return i_other.m_value == m_value; }
+	int get_in_degree() const { return  m_in_degree; }
+	void set_in_degree(int i_in_degree) { m_in_degree = i_in_degree; }
+	int get_out_degree() const { return  m_out_degree; }
+	void set_out_degree(int i_out_degree) { m_in_degree = i_out_degree; }
 };

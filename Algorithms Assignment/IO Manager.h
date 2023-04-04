a64f9638aeab73283b820a7e030f0bc40e6f04c1
Roy Toledano <royto@mta.ac.il>
2023-04-04 12:22:19 +0300

    bush

--- Algorithms Assignment/IO Manager.h
+++ Algorithms Assignment/IO Manager.h
@@ -13,5 +13,6 @@ public:
 	void get_graph_input(graph* i_graph);
 	void get_char_input(char& input);
 	void get_edge_input(int& src, int& dst, int num_of_vertexes);
+	void get_vertexes_and_edges_input(int& vertexes, int& edges);
 
 };
